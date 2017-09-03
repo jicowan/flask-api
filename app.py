4f14560c003dc91987d6649b0534437c1000c2a6
Jeremy Cowan <jicowan@users.noreply.github.com>
2017-09-03 07:27:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/todo', methods=['GET'])
 
 def get_query():
-    r = redis.StrictRedis(host='172.17.0.2', port=6379, db=0, password='0xJHtqj7wHlWQVm5Kjfepbk2UApdMQke')
+    r = redis.StrictRedis(host=redis-master, port=6379, db=0, password=$REDIS_PASSWORD)
     count = 50
     results = []
     grab = 42
